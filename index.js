8cc60a0c19a57f04d755648528e03a7217ca8de5
yulian rodriguez <rodriguezyulian16@gmail.com>
2023-09-01 23:14:08 -0600

    servidor express router

--- index.js
+++ index.js
@@ -4,18 +4,12 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const host = "localhost";
+const listTasks = require('./list-view-router');
+const listEdit = require ('./list-edit-router')
+app.use(express.json());
 
-// Definir la lista de tareas
-const tasks = [
-    { id: 1, descripcion: 'ir al gimnacio', estado: "pendiente" },
-    { id: 2, descripcion: 'capacitacion trabajo en altuuras', estado: "completado" },
-    { id: 3, descripcion: 'reunion familiar', estado: "pendiente" }
-  ];
-
-
-app.get('/', (req, res)=>{
-    res.status(200).send(tasks);
-  } )
+app.use('/tasks', listTasks);
+app.use('/tasks', listEdit);
 
 app.listen(port, host, ()=>{
     console.log("Servidor encendido");
---
+++ tareas.js
@@ -0,0 +1,6 @@
+// Definir la lista de tareas
+const tasks = [
+    { id: 1, descripcion: 'ir al gimnacio', estado: "pendiente" },
+    { id: 2, descripcion: 'capacitacion trabajo en altuuras', estado: "completado" },
+    { id: 3, descripcion: 'reunion familiar', estado: "pendiente" }
+  ];
